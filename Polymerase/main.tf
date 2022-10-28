f2cdfb30346554e3c957f824951780821fe3c515
sidanak <104363432+sidanak@users.noreply.github.com>
2022-10-28 15:15:18 +0530

    update plan

--- Polymerase/main.tf
+++ Polymerase/main.tf
@@ -39,13 +39,13 @@ resource "azurerm_linux_virtual_machine" "VM" {
   boot_diagnostics {
         storage_account_uri = var.StorageAccountid
     }
-/*
+
   plan {
-	name = "rhel-lvm85-gen2"
-	publisher = "RedHat"
+	name = "rhel-lvm85"
+	publisher = "redhat"
 	product = "rhel-byos"
   }
-*/
+
   tags = {
         Environment = "${var.tags_map.Environment}"
         "Cost Center" = "${var.tags_map.Cost_Center}"
